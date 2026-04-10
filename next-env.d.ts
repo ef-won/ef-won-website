da9d9db7228e22d8bf96bd97a6b46fd98d1aa0b7
ef-won <ef-won.com@hotmail.com>
2026-04-10 01:10:11 +0000

    layout error fixed

---
+++ next-env.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+declare module '*.css'
+declare module '*.module.css'
+declare module '*.scss'
+declare module '*.module.scss'
+declare module '*.sass'
+declare module '*.module.sass'
+declare module '*.less'
+declare module '*.module.less'
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
